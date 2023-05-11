458913397c7c719745f35f3951c59476238d3cf7
Jaime Carrazco Cajero <jaimecc0313@gmail.com>
2023-05-11 14:17:37 -0600

    seccion licenciaturas y posgrados
    acciones y funciones

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+document.getElementById("licenciatura").onclick = function () {
+    licenciatura.classList.add("selected");
+    posgrado.classList.remove("selected");
+};
+document.getElementById("posgrado").onclick = function () {
+    posgrado.classList.add("selected");
+    licenciatura.classList.remove("selected");
+};
+
+
+
+
+
+
+
+
